be4bb06d9dda47ea9f1e5824ca5d2b8fc3523b3b
juanFGR <juan.ramos.fg1@gmail.com>
2016-06-22 13:58:57 +0000

    init

--- platforms/android/assets/www/templates/themes/news-feed/service/services.js
+++
@@ -1,9 +0,0 @@
-appServices.factory('feed', function() {
-	return {
-	   	loadEvent: function(){
-	  	  var _event = Parse.Object.extend("Event");
-		  var query = new Parse.Query(_event);
-		  return query;
-	  	}
-	}
-});
